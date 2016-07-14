fc86b26590ecb3238979d571ebf0915a27f75430
Viacheslav Wolf <phoenixrvd@yahoo.de>
2016-07-15 00:05:43 +0200

    Code-Review

--- __init__.py
+++ __init__.py
@@ -4,8 +4,8 @@ import os
 
 from zim.actions import action
 from zim.applications import Application
-from zim.fs import UnixFile, File
-from zim.gui import FileDialog
+from zim.fs import File
+from zim.gui import FileDialog, GtkInterface
 from zim.plugins import PluginClass, WindowExtension, extends
 
 
@@ -16,6 +16,11 @@ class Dia(PluginClass):
         'author': 'Viacheslav Wolf',
         'help': 'Plugins:Dia',
     }
+
+    # TODO check_dependencies is dia installed ??? Windows ???
+
+    # TODO plugin_preferences for dia binary path configuration
+
 
 @extends('MainWindow')
 class MainWindowExtension(WindowExtension):
@@ -37,51 +42,87 @@ class MainWindowExtension(WindowExtension):
     )  # T: menu item
     def insert(self):
 
-        dialog = FileDialog(self.window, _('Select File')) # T: dialog title
-
-        # Set file filter for Dia-Types
-        filter = gtk.FileFilter()
-        filter.set_name(_('Dia'))
-        # T: Filter in open file dialog, shows image files only
-        filter.add_pixbuf_formats()
-        filter.add_pattern('*.dia') # allow only .dia extension
-        dialog.filechooser.add_filter(filter)
-        dialog.filechooser.set_filter(filter)
-
-        file = dialog.run()
+        file = DiaFileDialog(self.window).run()
         if file:
-            # TODO review
 
-            src_path = file.path
+            src_file, dest_file = DiaToSvg('dia', file, self.window.ui).convert()
 
-            m = hashlib.md5()
-            m.update(src_path)
-
-            page = self.window.ui.page
-            dir = self.window.ui.notebook.get_attachments_dir(page)
-            # Create dir if not exists
-            dir.touch()
-            dest_filenname = m.hexdigest()
-            dest_path = os.path.join(dir.path, dest_filenname + ".svg")
-
-            # TODO get from config
-            dot = Application( ('dia') )
-
-            # dia -t png --nosplash -e foo.png Diagramm1.dia
-            # TODO error handling and logging
-            dot.run( ('-t', 'svg', '--nosplash',  '-e', dest_path, file.path ) )
-
-            file = File(dest_path)
-
+            # TODO use relative path to image for src @see pageview.py L.6332
             attrib = {
-                'href': src_path,
-
-                # TODO Relative path to image @see pageview.py L.6332
-                'src': dest_path
+                'href': src_file.path,
+                'src': dest_file.path
             }
 
-            # Insert imge as link to local *.dia file
+            # Insert image as link to local *.dia file
             buffer = self.window.pageview.view.get_buffer()
             buffer.insert_at_cursor('\n')
-            buffer.insert_image_at_cursor(file, **attrib)
+            buffer.insert_image_at_cursor(dest_file, **attrib)
             buffer.insert_at_cursor('\n')
+
+
+class DiaFileDialog(FileDialog):
+
+    def __init__(self, window):
+        FileDialog.__init__(self, window, _('Select File')) # T: dialog title
+        self.add_filter(_('Dia'), '*.dia')
+
+    def add_filter(self, title, pattern):
+
+        file_type_filter = gtk.FileFilter()
+        file_type_filter.set_name(title)
+        file_type_filter.add_pattern(pattern)
+
+        self.filechooser.add_filter(file_type_filter)
+
+
+class DiaToSvg(Application):
+
+    image_type = 'svg'
+
+    def __init__(self, binary_path, src_file, ui):
+        """
+        :type binary_path: str
+        :type src_file: File
+        :type ui: GtkInterface
+        """
+
+        Application.__init__(self, binary_path)
+
+        self.ui = ui
+        self.src_file = src_file
+        self.dest_file = self.get_dest_file()
+
+    def get_dest_file(self):
+        """:rtype File"""
+
+        ui = self.ui
+        page = ui.page
+        dest_dir = ui.notebook.get_attachments_dir(page)
+        # Create dir if not exists
+        dest_dir.touch()
+
+        # image name is md5 from source path
+        m = hashlib.md5()
+        m.update(self.src_file.path)
+        dest_filenname = m.hexdigest() + "." + self.image_type
+
+        dest_path = os.path.join(dest_dir.path, dest_filenname)
+        dest_file = File(dest_path)
+        return dest_file
+
+    def convert(self):
+        src_file = self.src_file
+        dest_file = self.dest_file
+
+        args = (
+            '-t',
+            self.image_type,
+            '--nosplash',
+            '-e',
+            dest_file.path,
+            src_file.path,
+        )
+
+        # TODO throw error of not possible
+        self.run(args)
+        return src_file, dest_file
